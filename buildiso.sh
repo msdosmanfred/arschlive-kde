b04233ec445ffb77d6eb0130d07f026e39d9bc98
MS-DOS Manfred <hoefer9@gmail.com>
2023-12-25 12:37:43 +0100

    Updating ISO

--- buildiso.sh
+++ buildiso.sh
@@ -2,5 +2,5 @@
 if [ $UID -eq 0 ]; then
 	echo "You sudo'ed it! Please run this script without sudo!"
 else
-	sudo mkarchiso -v -w ~/iso/work -o ~/iso/output ~/iso/arschlinux-custom/releng/
+	sudo mkarchiso -v -w ~/iso/work -o ~/iso/output ~/iso/arschlive-kde/releng/
 fi
